79c202e35c6b430b564a4bf27d5f4fd79dd8c4b5
Filip Åhfelt <filahf94@gmail.com>
2019-04-12 14:05:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hellosensor/CompassActivity.java
@@ -0,0 +1,165 @@
+package com.example.hellosensor;
+
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+
+    ImageView arrow_img;
+    ImageView arrow_north_img;
+    TextView txt_compass;
+    int mAzimuth;
+    private SensorManager mSensorManager;
+    private Sensor mRotationV, mAccelerometer, mMagnetometer;
+    boolean haveSensor = false, haveSensor2 = false;
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        arrow_img = (ImageView) findViewById(R.id.img_arrow);
+        arrow_north_img = (ImageView) findViewById(R.id.img_arrow_north);
+        txt_compass = (TextView) findViewById(R.id.txt_azimuth);
+
+        start();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        mAzimuth = Math.round(mAzimuth);
+
+        niceNorth(mAzimuth);
+        displayWhere(mAzimuth);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public void start() {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
+                noSensorsAlert();
+            }
+            else {
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+        else{
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    public void noSensorsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device doesn't support the Compass.")
+                .setCancelable(false)
+                .setNegativeButton("Close",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    public void stop() {
+        if (haveSensor) {
+            mSensorManager.unregisterListener(this, mRotationV);
+        }
+        else {
+            mSensorManager.unregisterListener(this, mAccelerometer);
+            mSensorManager.unregisterListener(this, mMagnetometer);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        start();
+    }
+
+    public void niceNorth(int azi){
+        arrow_img.setRotation(-azi);
+        arrow_north_img.setRotation(-azi);
+
+        if(azi >= 350 || azi <= 10){
+            arrow_north_img.setVisibility(View.VISIBLE);
+            arrow_img.setVisibility(View.INVISIBLE);
+        } else{
+            arrow_north_img.setVisibility(View.INVISIBLE);
+            arrow_img.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void displayWhere(int azi){
+        String where = "NW";
+
+        if (azi >= 350 || azi <= 10)
+            where = "N";
+        if (azi < 350 && azi > 280)
+            where = "NW";
+        if (azi <= 280 && azi > 260)
+            where = "W";
+        if (azi <= 260 && azi > 190)
+            where = "SW";
+        if (azi <= 190 && azi > 170)
+            where = "S";
+        if (azi <= 170 && azi > 100)
+            where = "SE";
+        if (azi <= 100 && azi > 80)
+            where = "E";
+        if (azi <= 80 && azi > 10)
+            where = "NE";
+
+
+        txt_compass.setText(azi + "° " + where);
+    }
+
+}
